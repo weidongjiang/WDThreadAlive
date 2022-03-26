ee7c2fbd5ac49a58d2ae4981e9aede9bacbe1ed0
蒋伟东 <weidong17@satff.weibo.com>
2022-03-26 12:05:14 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-WDThreadAlive_Tests/Pods-WDThreadAlive_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WDThreadAlive_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WDThreadAlive_Tests
+@end
---
+++ Example/Pods/Target Support Files/WDThreadAlive/WDThreadAlive-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WDThreadAlive : NSObject
+@end
+@implementation PodsDummy_WDThreadAlive
+@end
